67c1f6d986fb284d1f82d87a8e4423720260035e
Buddhika <buddhika.ari@gmail.com>
2020-11-30 22:26:02 +0000

    Initial Commit

--- src/sortthreenumbers/SortThreeNumbers.java
+++ src/sortthreenumbers/SortThreeNumbers.java
@@ -5,31 +5,31 @@ import javax.swing.JOptionPane;
 public class SortThreeNumbers {
 
     public static void main(String[] args) {
-        Integer number1;
+        Integer number1 ;
         Integer number2;
         Integer number3;
-        Integer temporaryNumber;
-
+        Integer temporaryNumber ;
+        
         number1 = Integer.parseInt(JOptionPane.showInputDialog("Enter Number 1"));
         number2 = Integer.parseInt(JOptionPane.showInputDialog("Enter Number 2"));
         number3 = Integer.parseInt(JOptionPane.showInputDialog("Enter Number 3"));
-
-        if (number1 > number2) {
+        
+        if(number1>number2){
             temporaryNumber = number1;
             number1 = number2;
-            number2 = temporaryNumber;
+            number2=temporaryNumber;
         }
-        if (number1 > number3) {
+        if(number1>number3){
             temporaryNumber = number1;
             number1 = number3;
-            number3 = temporaryNumber;
+            number3=temporaryNumber;
         }
-        if (number2 > number3) {
+        if(number2>number3){
             temporaryNumber = number2;
             number2 = number3;
-            number3 = temporaryNumber;
+            number3=temporaryNumber;
         }
-
+        
         JOptionPane.showMessageDialog(null, "Ascensing Order is \n" + number1 + "\n" + number2 + "\n" + number3);
     }
 
